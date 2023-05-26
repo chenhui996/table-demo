5b801a098a927da7fda8b12af1eabed14dda4947
chenhui996 <chen562450669@qq.com>
2023-05-26 15:28:42 +0800

    feat: scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,86 @@
-function App() {
-  return (
-    <>
-      1
-    </>
-  );
+import React, { useState } from 'react';
+import VirtualTable, { Column } from './component/VirtualTable';
+
+interface DataItem {
+  id: number;
+  name: string;
+  age: string;
 }
+
+const rows: DataItem[] = [
+  { id: 1, name: 'John Doe1', age: '25' },
+  { id: 2, name: 'Jane Smith2', age: '30' },
+  { id: 3, name: 'Jane Smith3', age: '31' },
+  { id: 4, name: 'Jane Smith4', age: '32' },
+  { id: 5, name: 'Jane Smith5', age: '33' },
+  { id: 6, name: 'Jane Smith6', age: '34' },
+  { id: 7, name: 'Jane Smith7', age: '35' },
+  { id: 8, name: 'Jane Smith8', age: '36' },
+  { id: 9, name: 'Jane Smith9', age: '37' },
+  { id: 10, name: 'Jane Smith10', age: '38' },
+  { id: 11, name: 'Jane Smith11', age: '39' },
+  { id: 12, name: 'Jane Smith12', age: '39' },
+  { id: 13, name: 'Jane Smith13', age: '39' },
+  { id: 14, name: 'Jane Smith14', age: '39' },
+  { id: 15, name: 'Jane Smith15', age: '39' },
+  { id: 16, name: 'Jane Smith16', age: '39' },
+  { id: 17, name: 'Jane Smith17', age: '39' },
+  { id: 18, name: 'Jane Smith18', age: '39' },
+  { id: 19, name: 'Jane Smith19', age: '39' },
+  { id: 20, name: 'Jane Smith20', age: '39' }
+  // 更多数据...
+];
+
+const columns: Column[] = [
+  { dataIndex: 'id', title: 'ID', width: 100, textAlign: 'left' },
+  { dataIndex: 'name', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name1', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name2', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name3', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name4', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name5', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'age', title: 'Age', textAlign: 'left', width: 200 },
+  { dataIndex: 'name6', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name7', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name8', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name9', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name10', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name11', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name12', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name13', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name14', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'age', title: 'Age2', textAlign: 'left', width: 200 },
+  { dataIndex: 'name15', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name16', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name17', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name18', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name19', title: 'Name', textAlign: 'left', width: 200 },
+  { dataIndex: 'name20', title: 'Name', textAlign: 'left' },
+  { dataIndex: 'age', title: 'Age3', textAlign: 'left', width: 200 },
+  // 其他列设置
+];
+
+const App: React.FC = () => {
+  const [data, setData] = useState<any[]>(rows)
+
+  // setTimeout(() => {
+  //   const newData = [...data].concat([{
+  //     id: data.length + 1,
+  //     name: 'Jane Smith',
+  //     age: '40'
+  //   }]);
+
+  //   console.log('newData', newData);
+
+  //   setData(newData)
+  // }, 20);
+
+  return (
+    <div>
+      <h1 style={{color: '#fff'}}>Virtual Scrollable Table</h1>
+      <VirtualTable rows={data} rowHeight={30} visibleRows={10} columns={columns} />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/component/VirtualTable.tsx
@@ -0,0 +1,151 @@
+import React, { useState, useEffect, useRef, useMemo } from 'react';
+
+interface RowData {
+    id: number;
+    name: string;
+    age: string;
+    // 其他列数据
+}
+
+export interface Column {
+    dataIndex: string;
+    title: string;
+    width?: number;
+    textAlign?: 'left' | 'right' | 'center';
+    flexGrow?: number;
+    // 其他列属性
+}
+
+interface TableProps {
+    columns: Column[];
+    rows: RowData[];
+    rowHeight: number;
+    visibleRows: number;
+}
+
+// columns width
+const fitTableColumnsWidth = (columns: Column[], ref: React.RefObject<HTMLDivElement>, setColumns: React.Dispatch<React.SetStateAction<Column[]>>) => {
+    let totalWidth = columns.reduce((total, column) => {
+        if (column.width) {
+            return total + column.width;
+        }
+        return total;
+    }, 0);
+
+    const containerWidth = (ref as any).current?.clientWidth;
+    const remainingWidth = containerWidth - totalWidth; // 设置默认总宽度为100%，可根据实际情况调整
+
+    const flexibleColumns = (columns as Column[]).filter((column) => !column.width);
+    const flexibleWidth = Math.max(remainingWidth / flexibleColumns.length, 100);
+
+    const newColumns = (columns).map((column) => ({
+        ...column,
+        width: (column.width || flexibleWidth) - 2,
+    }))
+
+    totalWidth = newColumns.reduce((total, column) => {
+        if (column.width) {
+            return total + column.width + 2;
+        }
+        return total;
+    }, 0);
+
+    return {
+        newColumns,
+        containerWidth,
+        totalWidth
+    };
+}
+
+const VirtualTable: React.FC<TableProps> = ({ columns, rows, rowHeight, visibleRows }) => {
+    const [scrollTop, setScrollTop] = useState(0);
+    const [startIndex, setStartIndex] = useState(0);
+    const [visibleData, setVisibleData] = useState<any[]>([]);
+    const [containerWidth, setContainerWidth] = useState(0);
+    const tableRef = useRef<HTMLDivElement>(null);
+    const [adjustedColumns, setAdjustedColumns] = useState<Column[]>(columns);
+
+    const handleScroll = (event: React.UIEvent<HTMLDivElement>) => {
+        const { scrollTop } = event.currentTarget;
+        setScrollTop(scrollTop);
+    };
+
+    const totalHeight = rows.length * rowHeight; // 计算总高度
+    const [totalWidth, setTotalWidth] = useState(0);
+
+    // 设置 table columns --------------------------------------
+
+    useEffect(() => {
+        const { newColumns, containerWidth, totalWidth } = fitTableColumnsWidth(columns, tableRef, setAdjustedColumns); // columns width 相关
+
+        if (containerWidth) {
+            setContainerWidth(containerWidth);
+            setTotalWidth(totalWidth);
+            setAdjustedColumns(newColumns);
+        }
+    }, [columns])
+
+    // ---------------------------------------------------------
+
+    useEffect(() => {
+        if (tableRef.current) {
+            const newStartIndex = Math.floor(scrollTop / rowHeight);
+            setStartIndex(newStartIndex);
+        }
+    }, [scrollTop, rowHeight]);
+
+    useEffect(() => {
+        if (startIndex + visibleRows <= rows.length) {
+            setVisibleData(rows.slice(startIndex, startIndex + visibleRows));
+        }
+    }, [startIndex, visibleRows, rows])
+
+    return (
+        <div className="table-container" style={{ width: '100%', overflowX: 'auto' }} ref={tableRef}>
+            <div style={{ width: containerWidth, overflowX: 'scroll' }}>
+                <table className="table-header">
+                    <thead>
+                        <tr>
+                            {adjustedColumns.map((column) => (
+                                <th
+                                    key={column.dataIndex}
+                                    style={{ width: column.width, minWidth: column.width, textAlign: column?.textAlign || 'left' }}
+                                >
+                                    {column.title}
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                </table>
+                <div
+                    style={{ height: rowHeight * visibleRows, overflowY: 'scroll', width: totalWidth }} // 设置总高度和滚动属性
+                    onScroll={handleScroll}
+                    ref={tableRef}
+                >
+                    <div style={{ height: totalHeight, width: totalWidth, overflow: 'hidden' }}>
+                        <table className="table-body" style={{ paddingTop: startIndex * rowHeight }}>
+                            <tbody>
+                                {visibleData.map((row) => (
+                                    <tr key={row.id} style={{ height: rowHeight }}>
+                                        {adjustedColumns.map((column) => (
+                                            <td key={column.dataIndex} style={{ width: column.width, minWidth: column.width }}>
+                                                <div style={{
+                                                    width: column.width,
+                                                    textAlign: column.textAlign || 'left',
+                                                }}>
+                                                    {row[column.dataIndex]}
+                                                </div>
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default VirtualTable;
